444618d65060eebb9c3d327f2c96843e219e177b
koras_dor <koras_dor@yahoo.com>
2017-08-07 01:24:38 +0500

    add project

--- server/main.go
+++ server/main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-
+	"fmt"
 	"net/http"
+	"runtime"
+
+	"github.com/gorilla/mux"
+	"github.com/korasdor/colar/server/model"
 	"github.com/korasdor/colar/server/handler"
+	"github.com/korasdor/colar/server/services"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", 	handler.IndexHandler)
-	//r.HandleFunc("/static/media/{file}", handler.ServeStaticFiles)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	err := http.ListenAndServe(":8080", r)
+	m := model.New()
+	handler.Model = m
+
+	services.InitDb()
+	defer services.CloseDb()
+
+	bind := "127.0.0.1:8080"
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler.IndexHandler)
+	r.HandleFunc("/db_state", handler.DBStateHandler)
+	r.HandleFunc("/books", handler.BooksHandler)
+	r.HandleFunc("/update_books_template", handler.UpdateBooksHandler)
+	r.HandleFunc("/get_temp/{file_name}", handler.GetTempHandler)
+
+	r.HandleFunc("/create_serial/{serials_name}/{serials_count}", handler.CreateSerialsHandler)
+	r.HandleFunc("/get_serial/{serials_name}/{serials_format}", handler.GetSerialsHandler)
+	r.HandleFunc("/create_table/{serials_name}/{table_name}", handler.CreateTableHandler)
+	r.HandleFunc("/fill_table/{serials_name}/{table_name}/{dealer_id}", handler.FillTableHandler)
+
+	r.HandleFunc("/activate_serial/{table_name}/{serial_key}", handler.ActivateSerialsHandler)
+	r.HandleFunc("/reset_serial/{table_name}/{serial_key}", handler.ResetSerialsHandler)
+	r.HandleFunc("/about_serial/{table_name}/{serial_key}", handler.AboutSerialsHandler)
+
+	r.HandleFunc("/sendmail", handler.SendMailHandler)
+
+	//s := http.StripPrefix("/static/asset_bundles/", http.FileServer(http.Dir("./static/asset_bundles/")))
+	//r.PathPrefix("/static/asset_bundles/").Handler(s).HandlerFunc(handler.ServeBundle);
+	r.HandleFunc("/static/media/{file}", handler.ServeStaticFiles)
+
+	fmt.Printf("listening on %s...\n", bind)
+	err := http.ListenAndServe(bind, r)
 	if err != nil {
 		panic(err)
 	}
