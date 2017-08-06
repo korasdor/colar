e10369c1d7ad77c6e18fe448a81d0927bb6b9acb
koras_dor <koras_dor@yahoo.com>
2017-08-07 00:57:19 +0500

    fix handler

--- server/main.go
+++ server/main.go
@@ -8,10 +8,8 @@ import (
 )
 
 func main() {
-
-
 	r := mux.NewRouter()
-	r.HandleFunc("/", 	handler.IndexHandler)
+	r.HandleFunc("/", 	hello)
 	//r.HandleFunc("/static/media/{file}", handler.ServeStaticFiles)
 
 	err := http.ListenAndServe(":8080", r)
